f7036082210b054ceda81dc83a1f81148c274617
Jois <putujois0703@gmail.com>
2025-01-15 16:28:51 +0800

    update

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="mx-auto mt-10 md:mt-0 grid max-w-2xl grid-cols-1 text-gray-900 dark:text-slate-100 items-center gap-20 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8" v-for="profile in profiles">
+  <div
+    class="mx-auto mt-10 md:mt-0 grid max-w-2xl grid-cols-1 text-gray-900 dark:text-slate-100 items-center gap-20 px-4 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8"
+    v-for="profile in profiles"
+  >
     <div>
-      <div class="text-3xl font-bold sm:text-5xl">
-        {{ profile.salam }} {{ profile.name }}
-      </div>
+      <div class="text-3xl font-bold sm:text-5xl">{{ profile.salam }} {{ profile.name }}</div>
       <div class="text-3xl font-bold sm:text-5xl my-2">{{ profile.job }}</div>
       <div class="mt-4 text-xl"></div>
     </div>
@@ -11,7 +12,9 @@
     <div class="image flex justify-center items-center sm:w-80 w-56 mx-auto">
       <div class="profile">
         <img :src="profile.image" :alt="profile.alt" class="block rounded-t-lg" />
-        <div class="text-lg font-bold sm:w-80 w-56 bg-gray-800 dark:bg-gray-100 text-white dark:text-gray-900 text-center py-2 rounded-b-md">
+        <div
+          class="text-lg font-bold sm:w-80 w-56 bg-gray-800 dark:bg-gray-100 text-white dark:text-gray-900 text-center py-2 rounded-b-md"
+        >
           {{ profile.name }}
         </div>
       </div>
@@ -22,11 +25,11 @@
 <script setup>
 const profiles = [
   {
-    salam: "Halo ges gw ",
-    job: "Katana user",
-    name: "Blue Zero",
-    image: "elJamet1.jpg",
-    alt: "profile photo",
+    salam: 'Halo ges kenalin gw ',
+    job: 'Katana user',
+    name: 'Blue Zero',
+    image: 'elJamet1.jpg',
+    alt: 'profile photo',
   },
-];
+]
 </script>
